b58c21b856ece334337eab9e42ee90e889529d89
Minh <minhp1927@gmail.com>
2025-12-07 18:20:23 +0700

    chore: remove temp recovery files

--- recovery_temp/next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['example.com'], // Replace with your image domain
-  },
-  env: {
-    API_URL: process.env.API_URL, // Example of using environment variables
-  },
-};
